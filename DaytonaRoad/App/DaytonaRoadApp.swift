66b3f2d34b198d3fb5384752a2bf0c30397502a5
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-11 13:19:50 +0200

    init

---
+++ DaytonaRoad/App/DaytonaRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaytonaRoadApp.swift
+//  DaytonaRoad
+//
+//  Created by Serhii Babchuk on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DaytonaRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DRLaunchView()
+        }
+    }
+}
